b8f23cc178c6871247e299bc9ca22b398d1e624b
Corrie Smith & Kyle Mellander <student@epicodus.com>
2015-08-11 16:37:04 -0700

    Add feed and death options for tamagotchi

--- app.rb
+++ app.rb
@@ -22,5 +22,22 @@ get("/tamagotchi") do
   @food_level = @@tamagotchi.food_level()
   @happiness_level = @@tamagotchi.happiness_level()
   @energy_level = @@tamagotchi.energy_level()
-  erb(:tamagotchi)
+  if @food_level <= 0
+    erb(:dead)
+  else
+    erb(:tamagotchi)
+  end
+end
+
+get("/tamagotchi/feed") do
+  @@tamagotchi.check_time()
+  @@tamagotchi.feed()
+  @food_level = @@tamagotchi.food_level()  
+  @happiness_level = @@tamagotchi.happiness_level()
+  @energy_level = @@tamagotchi.energy_level()
+  if @food_level <= 0
+    erb(:dead)
+  else
+    erb(:tamagotchi)
+  end
 end
